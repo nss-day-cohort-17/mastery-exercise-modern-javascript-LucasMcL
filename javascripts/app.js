c9eb6a8044b24f060ef8cb819724972c43c59180
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-28 15:20:39 -0600

    notes and initial section layout

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,12 @@
-console.log("App")
+// SPA Layout
+
+// Every "page" in a section
+// Title page, click anywhere to continue
+// First page, next button on right side
+// Second page on, previous and next buttons
+
+// The previous button should execute immediately
+// The next button should perform a check to make sure
+//	something has been selected
+
+// Maybe give all the buttons that move forward the same class?
--- javascripts/robots.js
+++ javascripts/robots.js
@@ -1,1 +1,25 @@
-console.log("robots")
+// Required:
+  // 3 robot types
+    // 2 models per robot
+    // Different range of hp and attack per robot
+  // Simply display 6 options at beginning
+  // Attack button
+  // Message upon defeat
+  // Input name of player one and player two
+
+// Optional:
+	// Weapon selection
+	// Go back to beginning after fight
+	// Nice selection screen
+		// Clicking on robot or weapon highlights it
+		// Shows stats below
+		// Clicking next moves on
+	// One human player vs AI
+		// Real time
+		// Different weapons have different speeds
+		// Disable attack button while weapon recharges
+		// AI automatically attacks at speed of its weapon
+
+// Ideas:
+	//
+
